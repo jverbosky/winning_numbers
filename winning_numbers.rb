f4ae17cebe1acd237a3cc0d649e02067f7ee9608
jverbosky <jverbosk@gmail.com>
2017-01-25 17:25:05 -0500

    cleaned up function and added comments

--- winning_numbers.rb
+++ winning_numbers.rb
@@ -1,33 +1,49 @@
 # Determine if the specified number is in the specified array
 
+# Sandbox testing variables
+# number = 5
+# #number = 4
+# test_array = [1, 2, 3, 4, 5, 6, 7]
 
-
+# Method 1 - using .include?
 # def winning_numbers(num, set)
 # 	return set.include?(num)
 # end
 
-# Sandbox testing variables
-number = 5
-#number = 4
-test_array = [1, 2, 3, 4, 5, 6, 7]
+# Method 2 - using .empty? and clarification output
+# Jon advised that it's not good practice to set a boolean true to false (line 31)
+# def winning_numbers(num, set)
+# 	yes = []
+# 	no = []
+# 	counter = 1
+# 	set.each do |n|
+# 		if n == num
+# 			puts "If iteration #{counter}: n - #{n}, num - #{num}"
+# 			yes.push(n)
+# 			puts "If iteration - yes array: #{yes}, no array: #{no}"
+# 		else
+# 			puts "Else iteration #{counter}: n - #{n}, num - #{num}"
+# 			no.push(n)
+# 			puts "Else iteration - yes array: #{yes}, no array: #{no}"
+# 		end
+# 		counter += 1
+# 	end
+# 	if yes.empty? == false
+# 		return true
+# 	else
+# 		return false
+# 	end
+# end
 
+# Method 3 - dropped no array (not required) and compare .length of yes array for boolean
 def winning_numbers(num, set)
 	yes = []
-	no = []
-	counter = 1
 	set.each do |n|
 		if n == num
-			puts "If iteration #{counter}: n - #{n}, num - #{num}"
 			yes.push(n)
-			puts "If iteration - yes array: #{yes}, no array: #{no}"
-		else
-			puts "Else iteration #{counter}: n - #{n}, num - #{num}"
-			no.push(n)
-			puts "Else iteration - yes array: #{yes}, no array: #{no}"
 		end
-		counter += 1
 	end
-	if yes.empty? == false
+	if yes.length > 0
 		return true
 	else
 		return false
@@ -38,4 +54,4 @@ end
 # print test_array.include?(number)
 
 # Sandbox test to run function
-print winning_numbers(number, test_array)
+# print winning_numbers(number, test_array)
