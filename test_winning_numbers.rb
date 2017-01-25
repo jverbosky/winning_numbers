f240cbb465812768b6c0f08ffc8c4b88d3dd89ac
jverbosky <jverbosk@gmail.com>
2017-01-25 17:06:42 -0500

    rewrote function to not use include?

--- test_winning_numbers.rb
+++ test_winning_numbers.rb
@@ -17,9 +17,4 @@ class TestWinningNumbers < Minitest::Test
 		assert_equal(false, results)
 	end
 
-	def test_3_is_the_winning_number_in_the_new_array
-		results = winning_numbers(32830, [11829, 23891, 32830, 44983, 59203])
-		assert_equal([32830], results)
-	end
-
 end
--- winning_numbers.rb
+++ winning_numbers.rb
@@ -1,16 +1,41 @@
 # Determine if the specified number is in the specified array
 
+
+
+# def winning_numbers(num, set)
+# 	return set.include?(num)
+# end
+
 # Sandbox testing variables
-# number = 3
-# number = 4
-# test_array = [1, 2, 3]
+number = 5
+#number = 4
+test_array = [1, 2, 3, 4, 5, 6, 7]
 
 def winning_numbers(num, set)
-	return set.include?(num)
+	yes = []
+	no = []
+	counter = 1
+	set.each do |n|
+		if n == num
+			puts "If iteration #{counter}: n - #{n}, num - #{num}"
+			yes.push(n)
+			puts "If iteration - yes array: #{yes}, no array: #{no}"
+		else
+			puts "Else iteration #{counter}: n - #{n}, num - #{num}"
+			no.push(n)
+			puts "Else iteration - yes array: #{yes}, no array: #{no}"
+		end
+		counter += 1
+	end
+	if yes.empty? == false
+		return true
+	else
+		return false
+	end
 end
 
 # true if number is in the test_array, false if not
 # print test_array.include?(number)
 
 # Sandbox test to run function
-# print winning_numbers(number, test_array)
+print winning_numbers(number, test_array)
