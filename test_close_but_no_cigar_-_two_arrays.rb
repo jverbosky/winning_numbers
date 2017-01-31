1719221027403c75afb28425727699104eaa3bdf
jverbosk <jverbosk@gmail.com>
2017-01-31 15:14:29 -0500

    updated close_but_no_cigar - two_arrays third function and test for simpler requirements

--- test_close_but_no_cigar_-_two_arrays.rb
+++ test_close_but_no_cigar_-_two_arrays.rb
@@ -11,13 +11,13 @@ class TestCloseButNoCigarTwoArrays < Minitest::Test
   end
 
   def test_2_return_array_of_winning_numbers_with_one_digit_off_by_one
-    results = find_ticket_numbers_off_by_one(['1225', '1234', '5678', '6679'], ['6773', '2225', '6779', '1144', '1234', '1344'])
+    results = find_ticket_numbers_off_by_one(['1225', '1234', '5678', '6679'], ['6773', '2225', '6279', '1144', '1234', '1344'])
     assert_equal(['1225', '6679'], results)
   end
 
-  # def test_3_return_winning_numbers_with_no_matches_or_without_one_digit_off_by_one
-  #   results = find_unclaimed_winning_numbers(['1225', '1234', '5678', '6679'], ['6778', '2225', '6779', '1144', '1234', '1344'])
-  #   assert_equal(['6778', '1144', '1344'], results)
-  # end
+  def test_3_return_winning_numbers_with_no_matches_or_without_one_digit_off_by_one
+    results = find_unclaimed_winning_numbers(['1225', '1234', '5678', '6679'], ['6773', '2225', '6279', '1144', '1234', '1344'])
+    assert_equal(['6773', '1144', '1344'], results)
+  end
 
 end
